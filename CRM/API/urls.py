d72b4ec1949f339e9fd286212ff5d5450a016411
egums <dima110.ru@gmail.com>
2021-07-25 17:05:19 +0300

    Clients class done, but need testings

--- CRM/API/urls.py
+++ CRM/API/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import documentation, clients
+from .views import documentation, Clients
 
 urlpatterns = [
     path('documentation/', documentation, name="documentation_url"),
-    path('clients/<str:client_id>/', clients.show_one),
-    path('clients/<str:client_id>/delete/', clients.remove_client),
-    path('clients/create/<str:name>&<str:telegram>/', clients.create_client),
-    path('clients/create/', clients.create_client),
-    path('clients/', clients.show_all)
+    path('clients/<int:client_id>/change/<str:field>=<str:value>/', Clients.change_client),
+    path('clients/<int:client_id>/delete/', Clients.remove_client),
+    path('clients/<int:client_id>/', Clients.show_one),
+    path('clients/create/<str:name>&<str:telegram>/', Clients.create_client),
+    path('clients/create/', Clients.bad_request_check_documents),
+    path('clients/', Clients.show_all)
 ]
