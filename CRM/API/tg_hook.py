65f1489dcb21d8a95e53ec19e15365ac90e4f595
egums <dima110.ru@gmail.com>
2021-07-26 19:31:50 +0300

    adding tg_hook for notification

---
+++ CRM/API/tg_hook.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class telegram_notfication:
+    TELEGRAM_URL = "https://api.telegram.org/bot"
+    TUTORIAL_BOT_TOKEN = "1860214086:AAFw1RN499MRuTqVGUs9yY6mNOgLiZONetM"
+
+    def send_message(self, message, chat_id):
+        data = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "Markdown",
+        }
+        response = requests.post(
+            f"{self.TELEGRAM_URL}{self.TUTORIAL_BOT_TOKEN}/sendMessage", data=data
+        )
