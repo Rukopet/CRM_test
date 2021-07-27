6840a6aa2270b09ef4ee4669bd745ea52aadc2a1
egums <dima110.ru@gmail.com>
2021-07-27 10:39:23 +0300

    fix tg notifications

--- CRM/API/views.py
+++ CRM/API/views.py
@@ -117,14 +117,15 @@ class Bids(LoginRequiredMixin, APIView):
             elif field == "staff_id":
                 staff_id = StaffModel.objects.get(id=int(value))
                 obj.staff_id.set([staff_id])
-            elif field == "status":
-                if obj.notifications and obj.creator.all()[0].client_telegram_user_id not in [0, None]:
-                    telegram_notification.send_message("Your bid change your status",
-                                                       obj.creator.all()[0].client_telegram_user_id)
             elif field != "id":
                 obj[field] = str(value)
             else:
                 raise ValueError("Cant change id number")
+            if field == "status" or field == "staff_id":
+                if obj.notifications and obj.creator.all()[0].client_telegram_user_id not in [0, None]:
+                    telegram_notification.send_message(
+                        "Your bid change status" if field == "status" else "An executor has been assigned to your bid",
+                        obj.creator.all()[0].client_telegram_user_id)
             obj.save()
             return Response(status=200)
         except Exception as e:
